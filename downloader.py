0148704140436bff57c27a11511bb5778777aaea
Shengkai <chenshengkai@live.com>
2016-04-22 22:46:12 +0800

    ;;;

--- downloader.py
+++ downloader.py
@@ -1,27 +1,25 @@
 # -*- coding:utf-8 -*- 
 
 import tushare as ts
-import datetime as dt
 from sqlalchemy import create_engine, Date, text
 import pandas
 
-dd = Date()
-dd= '2016-01-04'
-print dd
 
-dayK = ts.get_h_data('600705', '2016-01-01', '2100-01-01', 'qfq')
-#dayK.to_csv('d:\\a.csv')
-
-
-#tickH = ts.get_tick_data('600008', '2004-01-05')
-#tickH.to_csv('d:\\b.csv')
-
-#dayK = pandas.read_csv('d:\\a.csv')
-
+_DAY = '_day'
+code = '600000'
+start_date = '1990-01-01'
+end_date = '2050-01-01'
+fuquan = 'qfq'
+table = code + _DAY + '_' + fuquan
+dayK = ts.get_h_data(code, start_date, end_date, fuquan)
 engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
-dayK.to_sql('dayk_table', engine, index=True, index_label='date', if_exists='replace', dtype={'date': Date})
-dayK1 = pandas.read_sql_query(text('SELECT open, high FROM dayk_table where date =: date1;'), engine, params={'date1': dd})
-print dayK1
+dayK.to_sql(table, engine, if_exists='replace', dtype={'date': Date})
+
+
+#read example
+#dayK1 = pandas.read_sql_query(text('SELECT open, high FROM "dayk_table" WHERE date =:date1;'), engine, params={'date1':'2016-01-04'})
+#dayK1 = pandas.read_sql_table(table, engine)
+
 
 
 
