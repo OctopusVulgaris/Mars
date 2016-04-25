b9053265f6581f0d96b73ce34522d82d3f1fc0dd
u0128583 <chenshengkai@live.com>
2016-04-25 17:54:32 +0800

    jlkj

--- __init__.py
+++ __init__.py
@@ -2,16 +2,28 @@
 
 import downloader
 
+trade_type_dic = {
+    '买盘' : 1,
+    '卖盘' : -1,
+    '中性盘' : 0
+}
+
 
 if __name__=="__main__":
     file_object = open('.\\conf\\sse_a.txt')
     list_of_all_the_lines = file_object.readlines()
     file_object.close()
 
-    downloader.create_dayk_talbe()
+    #downloader.create_dayk_talbe()
 
-    for line in list_of_all_the_lines:
-        print "requesting " + line.strip('\n') + "..."
-        downloader.request_instrument("dayk_qfq", line.strip('\n'))
+    #for line in list_of_all_the_lines:
+    #    print "requesting " + line.strip('\n') + "..."
+    #    downloader.request_instrument("dayk_qfq", line.strip('\n'))
+
+    import tushare as ts
+    a = ts.get_tick_data('600848', date='2016-04-24')
+    if a[0:1].time == '00:00:00':
+        print 'aaa'
+
 
 
--- downloader.py
+++ downloader.py
@@ -13,9 +13,30 @@ def create_dayk_talbe():
     cur.close()
     conn.close()
 
-def request_instrument(table, code, start_date = '1990-01-01', end_date = '2050-01-01', fuquan = 'qfq'):
+def request_dayk(table, code, start_date = '1990-01-01', end_date = '2050-01-01', fuquan = 'qfq'):
     dayK = ts.get_h_data(code, start_date, end_date, fuquan)
     dayK['code'] = code
+    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
+    dayK.to_sql(table, engine, if_exists='append', dtype={'date': Date})
+
+def create_tick_talbe(code):
+    conn = psycopg2.connect("dbname=test user=postgres password=postgres")
+    cur = conn.cursor()
+    sql = "create table if not exists " + code + "_tick_tbl (time timestamp primary key, price numeric, change numeric, volume numeric, amount numeric, type string, amount numeric, code integer)"
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def request_history_tick(table, code, start_date='1990-01-01', end_date='2050-01-01'):
+    create_tick_talbe(code)
+    tick = ts.get_h_data(code, start_date, end_date)
+
+    a = ts.get_tick_data('600848', date='2016-04-24')
+    a['type'] = a['type'].apply(lambda x: trade_type_dic[x])
+    a['time'] = '2014-01-09 ' + a['time']
+
+    print a[0:1].time
     engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
     dayK.to_sql(table, engine, if_exists='append', dtype={'date': Date})
 
