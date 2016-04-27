fe3093904941591724154cb3b9be55ee884d2d29
Shengkai <chenshengkai@live.com>
2016-04-27 16:55:25 +0800

    correct end date to fix, as it can't be done in one day

--- dataloader.py
+++ dataloader.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sqlalchemy as sa
 
 def get_code_list(start_code, end_code):
-    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test')
+    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres')
     sql = 'SELECT code, name FROM stock_list where CAST(code AS Integer) >= ' + start_code +' and CAST(code AS Integer) <= ' + end_code
 
     return pd.read_sql(sql, engine)
@@ -14,13 +14,13 @@ start_date = '1990-01-01'
 end_date = '2050-01-01'
 fuquan = 'qfq'
 table = code + _DAY + '_' + fuquan
-engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
+engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres', echo=True)
 #read example
 #dayK1 = pd.read_sql_query(text('SELECT open, high FROM "dayk_table" WHERE date =:date1;'), engine, params={'date1':'2016-01-04'})
 #dayK1 = pd.read_sql_table(table, engine, index_col = 'date')
 #dayK2 = dayK1.copy()
 
-#dayK1.to_sql('test', engine, if_exists='append')
+#dayK1.to_sql('postgres', engine, if_exists='append')
 #itr = dayK1.itertuples()
 #row = next(itr)
 #while row:
--- downloader.py
+++ downloader.py
@@ -13,7 +13,7 @@ logging.basicConfig(filename='log.txt', level=logging.DEBUG)
 
 
 def create_dayk_talbe():
-    conn = psycopg2.connect("dbname=test user=postgres password=postgres")
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
     cur = conn.cursor()
     cur.execute("create table if not exists dayk_qfq (date date, open numeric, high numeric, close numeric, low numeric, volume numeric, amount numeric, code integer)")
     conn.commit()
@@ -23,11 +23,11 @@ def create_dayk_talbe():
 def request_dayk(table, code, start_date = '1990-01-01', end_date = '2050-01-01', fuquan = 'qfq'):
     dayK = ts.get_h_data(code, start_date, end_date, fuquan)
     dayK['code'] = code
-    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
+    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres', echo=True)
     dayK.to_sql(table, engine, if_exists='append', dtype={'date': Date})
 
 def create_tick_talbe(code):
-    conn = psycopg2.connect("dbname=test user=postgres password=postgres")
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres")
     cur = conn.cursor()
     sql = "create table if not exists tick_tbl_" + code + " (index integer, time timestamp, price numeric, change text, volume numeric, amount numeric, type integer)"
     cur.execute(sql)
@@ -51,7 +51,7 @@ def change_dic(x):
 def request_history_tick(code, start_date='1995-01-01', end_date='2050-01-01'):
     create_tick_talbe(code)
 
-    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test')
+    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres')
     cur_day = datetime.datetime.strptime(start_date, '%Y-%m-%d')
     last_day = datetime.datetime.strptime(end_date, '%Y-%m-%d')
     logging.info('requesting code: ' + code + str(threading.currentThread()))
@@ -79,7 +79,7 @@ def request_history_tick(code, start_date='1995-01-01', end_date='2050-01-01'):
 
 def get_stock_basics():
     list = ts.get_stock_basics()
-    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/test', echo=True)
+    engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres', echo=True)
     list.sort_index(inplace=True)
     list.to_sql('stock_list', engine, if_exists='replace')
     return  list
