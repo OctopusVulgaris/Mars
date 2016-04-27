b52257dfe90ed96557cd53f43b3b168f9044c295
u0128583 <chenshengkai@live.com>
2016-04-27 17:35:22 +0800

    add func update_Stock_basics()
    will compare old and new versions, and write different code to ./conf/stock_list_*.txt

--- __init__.py
+++ __init__.py
@@ -26,21 +26,19 @@ threads.append(t5)
 print
 
 if __name__=="__main__":
-    file_object = open('.\\conf\\sse_a.txt')
-    list_of_all_the_lines = file_object.readlines()
-    file_object.close()
 
+    #downloader.update_stock_basics()
     #downloader.create_dayk_talbe()
 
     #for line in list_of_all_the_lines:
     #    print "requesting " + line.strip('\n') + "..."
     #    downloader.request_instrument("dayk_qfq", line.strip('\n'))
 
-    #for t in threads:
-    #    t.setDaemon(True)
-    #    t.start()
-    #t.join()
-    downloader.update_stock_basics()
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    t.join()
+
 
 
 
