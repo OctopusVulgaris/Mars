3fa00732168dabf72255a135c140e0e985f9277b
Shengkai <chenshengkai@live.com>
2016-05-05 22:27:14 +0800

    aa

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import sqlalchemy as sa
 import datetime
-import downloader
+import mydownloader
 import matplotlib.pyplot as plt
 from sqlalchemy import Date, text, DateTime, Integer
 
@@ -12,6 +12,7 @@ def to_date(x):
     return  x.date()
 
 def filter_group(x):
+    x = abs(x)
     if x > 5000000:
         return 500
     elif x > 1000000:
@@ -21,20 +22,21 @@ def filter_group(x):
     else:
         return 0
 
+
 engine = sa.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5432/postgres')
 
-code = '000869'
+code = '000700'
 #downloader.request_history_tick(code, engine, '2016-04-01', '2016-05-01')
 #downloader.request_dayk('dayk', code, engine, '2014-01-01', '2016-05-01')
 
 t1 = datetime.datetime.now()
 delta = datetime.timedelta(days=1)
-cur_day = datetime.datetime.strptime('2016-04-29', '%Y-%m-%d')
+cur_day = datetime.datetime.strptime('2016-01-01', '%Y-%m-%d')
 next_day = cur_day + delta
-last_day = datetime.datetime.strptime('2016-04-30', '%Y-%m-%d')
+last_day = datetime.datetime.strptime('2016-03-31', '%Y-%m-%d')
 
 
-sql = "SELECT time, price, change, volume, amount, type FROM tick_tbl_000869 where time > DATE '" + cur_day.strftime('%Y-%m-%d') + "' and time < DATE '" + last_day.strftime('%Y-%m-%d') +"'"
+sql = "SELECT time, price, change, volume, amount, type FROM tick_tbl_000700 where time > DATE '" + cur_day.strftime('%Y-%m-%d') + "' and time < DATE '" + last_day.strftime('%Y-%m-%d') +"'"
 one_year_tick = pd.read_sql(sql, engine, index_col='time', parse_dates={'time':'%Y-%m-%d'})
 
 print datetime.datetime.now()-t1
@@ -43,14 +45,28 @@ one_year_tick['volume'] = one_year_tick['amount'] / one_year_tick['price']
 one_year_tick['volume'] = one_year_tick['volume'].round()
 one_year_tick['amount'] = one_year_tick['amount'] * one_year_tick['type']
 one_year_tick['type'] = one_year_tick['amount'].apply(filter_group)
+#one_year_tick['500'] = one_year_tick['amount'].apply(filter_group_1, args=(500000,))
 
 #print one_year_tick
 #one_year_tick.to_sql('tick_tbl_' + code, engine, if_exists='replace', dtype={'time': DateTime})
 
 gg = one_year_tick.groupby([to_date, 'type']).sum()
+kk = one_year_tick.groupby([to_date, 'type']).mean()
 pd.set_option('display.multi_sparse', False)
 
-print gg.index.get_level_values(0)
+zz = pd.DataFrame()
+
+zz[0] = gg.loc(axis=0)[:,0]['amount'].reset_index(1)['amount']
+zz[50] = gg.loc(axis=0)[:,50]['amount'].reset_index(1)['amount']
+zz[100] = gg.loc(axis=0)[:,100]['amount'].reset_index(1)['amount']
+zz[500] = gg.loc(axis=0)[:,500]['amount'].reset_index(1)['amount']
+#zz['price'] = gg.loc(axis=0)[:,500]['price'].reset_index(1)['price']
+
+#print zz
+
+ax = zz.plot(rot= 70, grid=True, kind='bar')
+
+
 
 #mm['VWAP'] = sm.amount / sm.volume / 100
 #del mm['amount']
@@ -59,11 +75,9 @@ print gg.index.get_level_values(0)
 #del mm['type']
 
 #mm.plot()
-#plt.show()
+plt.show()
 
 print datetime.datetime.now()-t1
 
-import numpy as np
-arrays = [np.array(['bar', 'bar', 'baz', 'baz', 'foo', 'foo', 'qux', 'qux']), np.array(['one', 'two', 'one', 'two', 'one', 'two', 'one', 'two'])]
 
 
--- updateTickData.py
+++ updateTickData.py
@@ -17,15 +17,15 @@ def download_tick(start_code, end_code):
         row = next(itr)
 
 threads = []
-t1 = threading.Thread(target=download_tick, args=('000683','002133'))
+t1 = threading.Thread(target=download_tick, args=('000713','002133'))
 threads.append(t1)
-t2 = threading.Thread(target=download_tick, args=('002453','002737'))
+t2 = threading.Thread(target=download_tick, args=('002485','002737'))
 threads.append(t2)
-t3 = threading.Thread(target=download_tick, args=('300387','600060'))
+t3 = threading.Thread(target=download_tick, args=('300457','600060'))
 threads.append(t3)
-t4 = threading.Thread(target=download_tick, args=('600331','600764'))
+t4 = threading.Thread(target=download_tick, args=('600360','600764'))
 threads.append(t4)
-t5 = threading.Thread(target=download_tick, args=('601888','604000'))
+t5 = threading.Thread(target=download_tick, args=('603002','604000'))
 threads.append(t5)
 print
 
