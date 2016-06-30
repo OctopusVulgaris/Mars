e27dc59daebccb9200d9e4838b3a7e1e59ae2da2
andycheng1814 <chenglei1114@163.com>
2016-06-30 10:55:21 +0800

    b

--- utility.py
+++ utility.py
@@ -50,12 +50,6 @@ def get_realtime_all():
     riclist = dataloader.get_code_list('', '', engine)
     length = len(riclist)
     symbols =  riclist['code']
-    #symbols = []
-    #symbols.append('000001')
-    #symbols.append('600000')
-    #symbols.append('300026')
-    #df = ts.get_realtime_quotes(symbols[1000:1200])
-    #print df
     threads = []
     totalthreads = length / 300 + 1
     for i in range(0, totalthreads, 1):
@@ -81,8 +75,44 @@ def get_realtime_all():
     #full_df.to_csv('d:\\ut.csv',encoding='utf-8',index=False)
     return full_df
 
+def get_realtime_all_st(retry=60):
+    start = time.time()
+    riclist = dataloader.get_code_list('', '', engine)
+    length = len(riclist)
+    symbols =  riclist['code']
+    threads = []
+    loops = length / 300 + 1
+    for idx in range(0,loops,1):
+        sublist = symbols[idx * 300:(idx + 1) * 300]
+        for _ in range(retry):
+            try:
+                df = ts.get_realtime_quotes(sublist)
+            except Exception as e:
+                err = 'Error %s' % e
+                # print('Error %s' % e)
+                time.sleep(1)
+            else:
+                # print('get daily data for %s successfully' % row.code.encode("utf-8"))
+                break
+        global full_df
+        full_df = full_df.append(df)
+
+    finish = time.time()
+    print finish - start
+    full_df[['name','open','pre_close','price','date','code']] = full_df[['date','code','open','pre_close','price','name']]
+    names = full_df.columns.tolist()
+    names[names.index('name')] = 'Date'
+    names[names.index('open')] = 'Code'
+    names[names.index('pre_close')] = 'Open'
+    names[names.index('price')] = 'Pre_close'
+    names[names.index('date')] = 'Price'
+    names[names.index('code')] = 'Name'
+    full_df.columns = names
+    #full_df.to_csv('d:\\ut.csv',encoding='utf-8',index=False)
+    return full_df
+
 if __name__=="__main__":
-    df = get_realtime_all()
+    df = get_realtime_all_st()
     df.to_csv('d:\\ut.csv', encoding='utf-8', index=False)
     #df = get_today_all()
     #df.to_csv('d:\\ut_all.csv',encoding='utf-8',index=False)
