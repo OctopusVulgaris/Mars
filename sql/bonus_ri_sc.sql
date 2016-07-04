8fe18e14038dad9facf8bb00768fcbe6d54e321a
andycheng1814 <chenglei1114@163.com>
2016-07-04 14:04:02 +0800

    b

---
+++ sql/bonus_ri_sc.sql
@@ -0,0 +1,29 @@
+﻿-- Table: public.bonus_ri_sc
+
+-- DROP TABLE public.bonus_ri_sc;
+
+CREATE TABLE public.bonus_ri_sc
+(
+  code character varying(10) NOT NULL,
+  adate date NOT NULL,
+  give real NOT NULL DEFAULT 0,
+  transfer real NOT NULL DEFAULT 0,
+  paydiv real NOT NULL DEFAULT 0,
+  ri real NOT NULL DEFAULT 0,
+  riprice real DEFAULT 0,
+  basecap real DEFAULT 0,
+  rdate date,
+  xdate date NOT NULL,
+  reason character varying(30),
+  totalshare double precision DEFAULT 0.0,
+  tradeshare double precision NOT NULL DEFAULT 0.0,
+  limitshare double precision DEFAULT 0.0,
+  type character varying(20) NOT NULL,
+  prevts double precision DEFAULT 0.0,
+  CONSTRAINT pk_bsr PRIMARY KEY (code, adate, xdate, paydiv, ri, tradeshare)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.bonus_ri_sc
+  OWNER TO postgres;
---
+++ sql/weeklydata.sql
@@ -0,0 +1,29 @@
+﻿-- Table: public.weeklydata
+
+-- DROP TABLE public.weeklydata;
+
+CREATE TABLE public.weeklydata
+(
+  code character varying(10) NOT NULL,
+  date date NOT NULL,
+  name character varying(30),
+  close double precision NOT NULL DEFAULT 0.00,
+  high double precision,
+  low double precision,
+  open double precision,
+  netchng double precision,
+  pctchng double precision,
+  turnoverrate double precision,
+  vol double precision,
+  amo double precision,
+  totalcap double precision,
+  tradeablecap double precision,
+  prevclose numeric(6,2),
+  hfqratio double precision DEFAULT 1.00,
+  CONSTRAINT pk_wd PRIMARY KEY (code, date)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.weeklydata
+  OWNER TO postgres;
