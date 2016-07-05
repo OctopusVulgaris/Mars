26c5d0a94b5602902c6fdf66e7652ff896e341e1
Shengkai <chenshengkai@live.com>
2016-07-05 10:37:30 +0800

    aa

--- excute/BLSHPrepare.bat
+++ excute/BLSHPrepare.bat
@@ -1,2 +1,2 @@
-python ..\mydownloader.py -t delta
-python ..\buyLowSellHigh_r.py -t evening
+python D:\Github\Source\Mars\mydownloader.py -t delta
+python D:\Github\Source\Mars\buyLowSellHigh_r.py -t evening
--- excute/BLSHTrade.bat
+++ excute/BLSHTrade.bat
@@ -1,1 +1,1 @@
-python ..\buyLowSellHigh_r.py -t morning
+python D:\Github\Source\Mars\buyLowSellHigh_r.py -t morning
