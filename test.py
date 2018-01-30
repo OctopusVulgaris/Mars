741eef8ca8aa077b78dff9e794850ebc86060103
u0128583 <chenshengkai@live.com>
2018-01-30 17:36:18 +0800

    qq

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 
 import datetime as dt
 import pandas as pd
+import talib as ta
+
+ta.LINEARREG_SLOPE()
 
 c = pd.bdate_range(start='2017-12-31', end='2018-12-31')
 holidays=[dt.date(2018,1,1),dt.date(2018,2,15),dt.date(2018,2,16),dt.date(2018,2,19),dt.date(2018,2,20),dt.date(2018,2,21),dt.date(2018,4,5),dt.date(2018,4,6),dt.date(2018,4,30),dt.date(2018,5,1),dt.date(2018,6,18),dt.date(2018,9,24),dt.date(2018,10,1),dt.date(2018,10,2),dt.date(2018,10,3),dt.date(2018,10,4),dt.date(2018,10,5)]
